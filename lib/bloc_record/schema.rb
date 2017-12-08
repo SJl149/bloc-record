32d0f4d946adb0eea0c394ad72c1e4975143eb7e
ScottL <sjlteacher@gmail.com>
2017-12-08 15:41:35 -0500

    BlocRecord created.

---
+++ lib/bloc_record/schema.rb
@@ -0,0 +1,28 @@
+require 'sqlite3'
+require 'bloc_record/utility'
+
+module Schema
+  def table
+    BlocRecord::Utility.underscore(name)
+  end
+
+  def schema
+    @schema = {}
+    connection.table_info(table) do |col|
+      @schema[col["name"]] = col["type"]
+    end
+    @schema
+  end
+
+  def columns
+    schema.keys
+  end
+
+  def attributes
+    columns - ["id"]
+  end
+
+  def count
+    connection.execute("SELECT COUNT(*) FROM #{table}")[0][0]
+  end
+end
