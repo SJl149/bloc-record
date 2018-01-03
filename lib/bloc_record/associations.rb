b96513266eda34bdc7dd572d32e5a898974b4765
ScottL <sjlteacher@gmail.com>
2018-01-03 09:13:19 -0500

    Checkpoint Associations assignment is complete.

--- lib/bloc_record/associations.rb
+++ lib/bloc_record/associations.rb
@@ -1,4 +1,5 @@
 require 'sqlite3'
+require 'pg'
 require 'active_support/inflector'
 
 module Associations
@@ -20,6 +21,22 @@ module Associations
     end
   end
 
+  def has_one(association)
+    define_method(association) do
+      row = self.class.connection.get_first_row <<-SQL
+        SELECT * FROM #{association.to_s}
+        WHERE #{self.class.table}_id = #{self.id};
+      SQL
+
+      class_name = association.to_s.classify.constantize
+
+      if row
+        data = Hash[class_name.columns.zip(row)]
+        class_name.new(data)
+      end
+    end
+  end
+
   def belongs_to(association)
     define_method(association) do
       association_name = association.to_s
@@ -36,5 +53,5 @@ module Associations
       end
     end
   end
-  
+
 end
