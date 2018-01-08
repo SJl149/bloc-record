d18c3ba1865c64267c98d7bf2dc3861fa009a82a
ScottL <sjlteacher@gmail.com>
2018-01-08 17:59:20 -0500

    Moved where, take, and to Collection.

--- lib/bloc_record/collection.rb
+++ lib/bloc_record/collection.rb
@@ -5,9 +5,60 @@ module BlocRecord
       self.any? ? self.first.class.update(ids, updates) : false
     end
 
+    def take
+      ids = self.map(&:id)
+      row = connection.get_first_row <<-SQL
+        SELECT #{columns.join ","} FROM #{table}
+        ORDER BY random()
+        LIMIT 1;
+      SQL
+
+      init_object_from_row(row)
+    end
+
+    def where(*args)
+      if args.count > 1
+        expression = args.shift
+        params = args
+      else
+        case args.first
+        when String
+          expression = args.first
+        when Hash
+          expression_hash = BlocRecord::Utility.convert_keys(args.first)
+          expression = expression_hash.map { |key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}"}.join(" and ")
+        end
+      end
+
+      sql = <<-SQL
+        SELECT #{columns.join ","} FROM #{table}
+        WHERE #{expression};
+      SQL
+
+      rows = connection.execute(sql, params)
+      rows_to_array(rows)
+    end
+
     def not(args)
+      experession = args.shift
+      params = args
       ids = self.map(&:id)
       self.any? ? self.first.class.where(ids, args) : false
     end
+
+    private
+    def init_object_from_row(row)
+      if row
+        data = Hash[columns.zip(row)]
+        new(data)
+      end
+    end
+
+    def rows_to_array(rows)
+      collection = BlocRecord::Collection.new
+      rows.each { |row| collection << new(Hash[columns.zip(row)]) }
+      collection
+    end
+
   end
 end
--- lib/bloc_record/persistence.rb
+++ lib/bloc_record/persistence.rb
@@ -28,64 +28,7 @@ module Persistence
     true
   end
 
-  def take(num=1)
-    if num > 1
-      rows = connection.execute <<-SQL
-        SELECT #{columns.join ","} FROM #{table}
-        ORDER BY random()
-        LIMIT #{num};
-      SQL
 
-      rows_to_array(rows)
-    else
-      take_one
-    end
-  end
-
-  def take_one
-    row = connection.get_first_row <<-SQL
-      SELECT #{columns.join ","} FROM #{table}
-      ORDER BY random()
-      LIMIT 1;
-    SQL
-
-    init_object_from_row(row)
-  end
-
-  def where(*args)
-    if args.count > 1
-      expression = args.shift
-      params = args
-    else
-      case args.first
-      when String
-        expression = args.first
-      when Hash
-        expression_hash = BlocRecord::Utility.convert_keys(args.first)
-        expression = expression_hash.map { |key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}"}.join(" and ")
-      end
-    end
-
-    sql = <<-SQL
-      SELECT #{columns.join ","} FROM #{table}
-      WHERE #{expression};
-    SQL
-
-    rows = connection.execute(sql, params)
-    rows_to_array(rows)
-  end
-
-  def not(arg)
-    case arg
-    when String
-      expression = arg
-    when Hash
-      expression_hash = BlocRecord::Utility.convert_keys(arg)
-      expression = expression_hash.map { |key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}"}.join(" AND ")
-    end
-    
-    "NOT " + expression
-  end
 
   def update_attribute(attribute, value)
     self.class.update(self.id, { attribute => value })
@@ -146,17 +89,4 @@ module Persistence
 
   end
 
-  private
-  def init_object_from_row(row)
-    if row
-      data = Hash[columns.zip(row)]
-      new(data)
-    end
-  end
-
-  def rows_to_array(rows)
-    collection = BlocRecord::Collection.new
-    rows.each { |row| collection << new(Hash[columns.zip(row)]) }
-    collection
-  end
 end
